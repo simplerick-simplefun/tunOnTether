046b613802c80a9932fb65202bf5a653970d8759
simplerick-simplefun <13503950+simplerick-simplefun@users.noreply.github.com>
2024-05-18 11:18:13 -0400

    v1.1 version bump

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -r -o -X -ll tunOnTether_$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'tot.json'
+zip -r -o -X -ll tunOnTether_$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'tot.json' -x '.git*'
